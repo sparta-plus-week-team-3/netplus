46c0801eba9142b009e2f6d6aa6f94d2ad3ccdbf
cnux9 <cnux9@naver.com>
2025-01-31 20:47:32 +0900

    feat: 인기 검색어 조회

---
+++ src/main/java/com/example/com/netplus/controller/search/SearchController.java
@@ -0,0 +1,27 @@
+package com.example.com.netplus.controller.search;
+
+import com.example.com.netplus.service.search.SearchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/search")
+@RestController
+@RequiredArgsConstructor
+public class SearchController {
+    private final SearchService searchService;
+
+    @PostMapping
+    public ResponseEntity<Void> search(@RequestParam("q") String queryString) {
+        searchService.search(queryString);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/popular")
+    public ResponseEntity<List<String>> findTopTenQueries() {
+        return ResponseEntity.ok(searchService.findTopTenQueries());
+    }
+}
---
+++ src/main/java/com/example/com/netplus/entity/search/SearchQuery.java
@@ -0,0 +1,19 @@
+package com.example.com.netplus.entity.search;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "query")
+public class SearchQuery {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long queryId;
+    private String content;
+//    private Long count;
+}
