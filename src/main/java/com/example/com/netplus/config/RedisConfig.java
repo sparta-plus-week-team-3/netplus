99a4316b99eb4f5f7026233f604fc629430b3bc2
cnux9 <cnux9@naver.com>
2025-02-05 21:59:45 +0900

    fix: Value 타입이 String과 Integer인 RedisTemplate 빈 따로 등록

--- src/main/java/com/example/com/netplus/config/RedisConfig.java
+++ src/main/java/com/example/com/netplus/config/RedisConfig.java
@@ -4,24 +4,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+//    @Bean(name = "stringRedisTemplate")
+//    public RedisTemplate<String, String> stringRedisTemplate(RedisConnectionFactory connectionFactory) {
+//        RedisTemplate<String, String> template = new RedisTemplate<>();
+//        template.setConnectionFactory(connectionFactory);
+//
+//        StringRedisSerializer stringSerializer = new StringRedisSerializer();
+//
+//        template.setKeySerializer(stringSerializer);
+//        template.setValueSerializer(stringSerializer);
+//
+//        template.setHashKeySerializer(stringSerializer);
+//        template.setHashValueSerializer(stringSerializer);
+//
+//        template.afterPropertiesSet();
+//        return template;
+//    }
+
     @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, String> template  = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
-
-        StringRedisSerializer stringSerializer = new StringRedisSerializer();
-
-        template.setKeySerializer(stringSerializer);
-        template.setValueSerializer(stringSerializer);
-
-        template.setHashKeySerializer(stringSerializer);
-        template.setHashValueSerializer(stringSerializer);
-
-        template.afterPropertiesSet();
+    public RedisTemplate<String, Integer> integerRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Integer> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
         return template;
     }
 }
