74fa0d68be071ddeedebd0ae314868b7199e1d61
taejunUM <utj0411@naver.com>
2025-02-07 02:43:30 +0900

    refactor. DTO Class type change class > record

--- src/main/java/com/example/com/netplus/dto/content/request/UpdateRequest.java
+++ src/main/java/com/example/com/netplus/dto/content/request/UpdateRequest.java
@@ -1,13 +1,10 @@
 package com.example.com.netplus.dto.content.request;
 
 import com.example.com.netplus.common.Category;
-import lombok.Builder;
-import lombok.Getter;
 
-@Getter
-@Builder
-public class UpdateRequest {
-    private String title;
-    private String description;
-    private Category category;
+public record UpdateRequest(
+        String title,
+        String description,
+        Category category
+) {
 }
--- src/main/java/com/example/com/netplus/dto/content/response/ContentWithViewCountResponse.java
+++ src/main/java/com/example/com/netplus/dto/content/response/ContentWithViewCountResponse.java
@@ -1,11 +1,7 @@
 package com.example.com.netplus.dto.content.response;
 
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-@Builder
-public class ContentWithViewCountResponse {
-    private final ContentResponse content;
-    private final int viewCount;
+public record ContentWithViewCountResponse(
+        ContentResponse content,
+        int viewCount
+) {
 }
